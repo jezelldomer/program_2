540a385a14a50bbbf5ad07e16c31b60930bb99bb
Jezell <jzlldomer@gmail.com>
2021-10-28 00:35:59 +0800

    third commit

--- total-amount.py
+++ total-amount.py
@@ -1,2 +1,3 @@
-question = input("Enter how many apples you want to buy:")
+question1 = input("Enter how many apples you want to buy:")
+question2 = input("Enter how many oranges you want to buy:")
 
