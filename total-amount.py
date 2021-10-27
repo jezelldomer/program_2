78dd0995d0e79b07d31e2ff60dfb92acd7729c05
Jezell <jzlldomer@gmail.com>
2021-10-28 00:51:42 +0800

    print

--- total-amount.py
+++ total-amount.py
@@ -1,3 +1,6 @@
 question1 = input("Enter how many apples you want to buy:")
 question2 = input("Enter how many oranges you want to buy:")
-
+apple = 20 
+orange = 25
+result = int(apple) * int(question1) + int(orange) * int(question2)
+print("The total amount is ", result )
