39b3db47645c9ea391cdb7744ccb77f349924eec
Jezell <jzlldomer@gmail.com>
2021-10-27 23:29:05 +0800

    address

--- name-age-address.py
+++ name-age-address.py
@@ -1,4 +1,4 @@
 name = input("Enter your name:")
 age = input("Enter your age:")
 address = input("Enter your address:")
-input("Hi, my name is " + name, "I am " + age,)
+input("Hi, my name is " + name, "I am " + age, "and I live in " + address )
