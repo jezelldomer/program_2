f54cb54e88cc5f7e2d00bdab38be69ebffe8bdc4
Jezell <jzlldomer@gmail.com>
2021-10-28 14:49:50 +0800

    print

--- amount-apples.py
+++ amount-apples.py
@@ -5,3 +5,5 @@ result = (float(money) / float(amount))
 result_2 = (int(result) * int(amount))
 result_3 = (float(money)) - (float(result_2)) 
 
+print ("You can buy", int(result), "apples", "And your change is ", (result_3))
+
