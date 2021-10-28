19f33e1c9e976ef7296d4ff45bfa40c24daaa46b
Jezell <jzlldomer@gmail.com>
2021-10-28 14:48:06 +0800

    results_all

--- amount-apples.py
+++ amount-apples.py
@@ -1,3 +1,7 @@
 money = input("Enter the amount of money you have:")
 amount = input("Enter the price of the apple:")
-result = (int(money) / int(amount)) 
+
+result = (float(money) / float(amount)) 
+result_2 = (int(result) * int(amount))
+result_3 = (float(money)) - (float(result_2)) 
+
